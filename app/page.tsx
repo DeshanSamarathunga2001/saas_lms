8ac38a02b978b839794ae96d386f4ddec3a0740b
DeshanSamarathunga2001 <deshansamarathunga13@gmail.com>
2025-12-06 22:57:08 +0530

    Add companion components and update homepage layout
    Introduced CompanionCard, CompanionsList, and CTA components. Updated the homepage to display popular companions using CompanionCard and included the new components in the layout for improved structure and functionality.

--- app/page.tsx
+++ app/page.tsx
@@ -1,14 +1,44 @@
 import React from 'react'
 import {Button} from "@/components/ui/button";
+import CompanionCard from "@/components/CompanionCard";
+import CompanionsList from "@/components/CompanionsList";
+import CTA from "@/components/CTA";
 
 const Page = () => {
   return (
-    <div>
-      <h1 className='text-2xl underline'>Welcome To my SaaS</h1>
-      <Button>
-        Let`s Get Start
-      </Button>
-    </div>
+    <main>
+      <h1>Popular Companions</h1>
+      <section className="home-section">
+          <CompanionCard
+            id = "123"
+            name = "Neura the Brainy Explorer"
+            topic = "Neura Network of the Brain"
+            subject = "science"
+            duration = {45}
+            color = "#ffda6e"
+          />
+          <CompanionCard
+              id = "456"
+              name = "Countsy the Number Wizard"
+              topic = "Derivatives & Integrals"
+              subject = "science"
+              duration = {30}
+              color = "#e5d0ff"
+          />
+          <CompanionCard
+              id = "789"
+              name = "Verba the Vocabulary Builder"
+              topic = "Language"
+              subject = "English "
+              duration = {30}
+              color = "#BDE7ff"
+          />
+      </section>
+      <section className="home-section">
+          <CompanionsList/>
+          <CTA/>
+      </section>
+    </main>
   )
 }
 
