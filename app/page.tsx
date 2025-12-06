4c594e4421eea491c14645e824f0161d304872b6
DeshanSamarathunga2001 <deshansamarathunga13@gmail.com>
2025-12-06 18:36:50 +0530

    Initial project setup with Next.js and UI components
    Bootstrap Next.js app with essential configuration, global styles, UI button component, constants, utility functions, and initial page layout. Includes .gitignore, IDE settings, public assets, and basic documentation.

---
+++ app/page.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import {Button} from "@/components/ui/button";
+
+const Page = () => {
+  return (
+    <div>
+      <h1 className='text-2xl underline'>Welcome To my SaaS</h1>
+      <Button>
+        Let`s Get Start
+      </Button>
+    </div>
+  )
+}
+
+export default Page
