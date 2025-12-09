36c8e0a97fc7dd56888bbe6b737d6cbaf3c45610
DeshanSamarathunga2001 <deshansamarathunga13@gmail.com>
2025-12-09 10:11:55 +0530

    Done

--- next.config.ts
+++ next.config.ts
@@ -5,6 +5,11 @@ const nextConfig: NextConfig = {
         NEXT_PUBLIC_SUPABASE_URL: process.env.NEXT_PUBLIC_SUPABASE_URL,
         NEXT_PUBLIC_SUPABASE_ANON_KEY: process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY,
     },
+    images: {
+        remotePatterns: [
+            { hostname: 'img.clerk.com'}
+        ]
+    }
 };
 
 export default nextConfig;
